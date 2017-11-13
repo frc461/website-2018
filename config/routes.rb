b89e9f3c2650d91b158615fabff2b85a4f953e6d
jenkinsmichpa <jenkinsmichpa@gmail.com>
2017-11-13 18:11:12 -0500

    Did some things

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
   root 'static_pages#home'
+  get 'sponsors', to: 'static_pages#sponsors'
+  get 'aboutus', to: 'static_pages#aboutus'
+  get 'aboutfirst', to: 'static_pages#aboutfirst'
+  get 'subteams', to: 'static_pages#subteams'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
